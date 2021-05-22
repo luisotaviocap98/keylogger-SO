c5cbfa76e16700db7f20ebbb4b4657f071a82bd8
luisotaviocap98 <luiscapelari@alunos.utfpr.edu.br>
2021-05-22 16:10:14 -0300

    mapeando teclado

--- modulo.c
+++ modulo.c
@@ -4,54 +4,152 @@
 #include <linux/kernel.h>   
 #include <linux/proc_fs.h> 
 #include <linux/fs.h>
+#include <linux/keyboard.h>
+#include <linux/debugfs.h>
+#include <linux/input.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Luis Otavio O.C, Pedro P.");
  
 //  receber parametros
-static char arquivo[BUF_LEN];
-module_param_string(arquivo, arq, BUF_LEN, 0);
+// static char arquivo[BUF_LEN];
+// module_param_string(arquivo, arq, BUF_LEN, 0);
 
 
-// sera passado um nome de arquivo
-// pegar o ponteiro de arquivo
-// descobrir inode dele
-// pegar informaçoes do inode
-
-static struct inode *get_info(struct inode *inode)
-{
-	umode_t modo = inode->i_mode; //permissoes
-	uid_t user = inode->i_uid; //user id owner
-	loff_t tam = inode->i_size; //tamanho arquivo
-	struct timespec acesso = inode->i_atime; //ultimo acesso
- 	struct inode_operations *iop = inode->i_op; //operacoes de inode
-	struct file_operations *fop = inode->i_fop; //operacoes de arquivo 
-	struct super_block *sblk = inode->i_sb; //superbloco
+// mapeando as teclas do teclado de acordo com scancode
+static const char *teclado[][2] = {
+	{"\0", "\0"}, 						//0
+	{"_ESC_", "_ESC_"}, 
+	{"1", "!"}, 
+	{"2", "@"},       
+	{"3", "#"}, 
+	{"4", "$"}, 
+	{"5", "%"}, 
+	{"6", "¨"},                 
+	{"7", "&"}, 
+	{"8", "*"}, 
+	{"9", "("}, 						//10
+	{"0", ")"},                 
+	{"-", "_"}, 
+	{"=", "+"}, 
+	{"_BACKSPACE_", "_BACKSPACE_"},         
+	{"_TAB_", "_TAB_"}, 
+	{"q", "Q"}, 
+	{"w", "W"}, 
+	{"e", "E"}, 
+	{"r", "R"},
+	{"t", "T"}, 						//20
+	{"y", "Y"}, 
+	{"u", "U"}, 
+	{"i", "I"},                 
+	{"o", "O"}, 
+	{"p", "P"},
+	{"´", "`"}, 
+	{"[", "{"},                  
+	{"\n", "\n"}, 
+	{"_LCTRL_", "_LCTRL_"}, 
+	{"a", "A"}, 						//30
+	{"s", "S"},   
+	{"d", "D"}, 
+	{"f", "F"}, 
+	{"g", "G"}, 
+	{"h", "H"},                 
+	{"j", "J"}, 
+	{"k", "K"}, 
+	{"l", "L"}, 
+	{"ç", "Ç"},                 
+	{"~", "^"}, 						//40
+	{"'", "\""}, 
+	{"_LSHIFT_", "_LSHIFT_"}, 
+	{"]", "}"}, 
+	{"z", "Z"}, 
+	{"x", "X"}, 
+	{"c", "C"}, 
+	{"v", "V"},                 
+	{"b", "B"}, 
+	{"n", "N"}, 
+	{"m", "M"}, 						//50
+	{",", "<"},                 
+	{".", ">"}, 
+	{";", ":"}, 
+	{"_RSHIFT_", "_RSHIFT_"}, 
+	{"_KPD*_", "_KPD*_"},
+	{"_LALT_", "_LALT_"}, 
+	{" ", " "}, 
+	{"_CAPS_", "_CAPS_"}, 
+	{"F1", "F1"},
+	{"F2", "F2"}, 						//60
+	{"F3", "F3"}, 
+	{"F4", "F4"}, 
+	{"F5", "F5"},         
+	{"F6", "F6"}, 
+	{"F7", "F7"}, 
+	{"F8", "F8"}, 
+	{"F9", "F9"},         
+	{"F10", "F10"}, 
+	{"_NUM_", "_NUM_"}, 
+	{"_SCROLL_", "_SCROLL_"},   		//70
+	{"_KPD7_", "_HOME_"}, 
+	{"_KPD8_", "_UP_"}, 
+	{"_KPD9_", "_PGUP_"}, 
+	{"-", "-"}, 
+	{"_KPD4_", "_LEFT_"}, 
+	{"_KPD5_", "_KPD5_"},         
+	{"_KPD6_", "_RIGHT_"}, 
+	{"+", "+"}, 
+	{"_KPD1_", "_END_"},         
+	{"_KPD2_", "_DOWN_"}, 				//80
+	{"_KPD3_", "_PGDN"}, 
+	{"_KPD0_", "_INS_"}, 
+	{"_KPD._", "_DEL_"}, 
+	{"_PRTSCR_", "_PRTSCR_"}, 
+	{"\0", "\0"},      
+	{"\\", "|"}, 
+	{"F11", "F11"}, 
+	{"F12", "F12"}, 
+	{"\0", "\0"},     
+	{"\0", "\0"}, 						//90
+	{"\0", "\0"}, 
+	{"\0", "\0"}, 
+	{"\0", "\0"}, 
+	{"\0", "\0"},
+	{"\0", "\0"}, 
+	{"_KPENTER_", "_KPENTER_"}, 
+	{"_RCTRL_", "_RCTRL_"}, 
+	{"/", "/"},
+	{"_PRTSCR_", "_PRTSCR_"}, 
+	{"_RALT_", "_RALT_"}, 				//100
+	{"\0", "\0"},   
+	{"_HOME_", "_HOME_"}, 
+	{"_UP_", "_UP_"}, 
+	{"_PGUP_", "_PGUP_"},   
+	{"_LEFT_", "_LEFT_"}, 
+	{"_RIGHT_", "_RIGHT_"}, 
+	{"_END_", "_END_"},
+	{"_DOWN_", "_DOWN_"}, 
+	{"_PGDN", "_PGDN"}, 
+	{"_INS_", "_INS_"},   				//110
+	{"_DEL_", "_DEL_"}, 
+	{"\0", "\0"}, 
+	{"\0", "\0"}, 
+	{"\0", "\0"},   
+	{"/", "?"}, 
+	{"\0", "\0"}, 
+	{"\0", "\0"}, 
+	{"\0", "\0"},         
+	{"_PAUSE_", "_PAUSE_"},           	         
+	{"\0", "\0"},						//120
+	{"\0", "\0"},
+	{"\0", "\0"},
+	{"\0", "\0"},
+	{"\0", "\0"},
+	{"\0", "\0"},
+	{".", "."}              
 };
 
-static struct inode *V1_minix_iget(struct inode *inode)
-{
-      struct buffer_head * bh;
-      struct minix_inode * raw_inode;
-      struct minix_inode_info *minix_inode = minix_i(inode);
-      int i;
 
-      raw_inode = minix_V1_raw_inode(inode->i_sb, inode->i_ino, &bh);
-      if (!raw_inode) {
-              iget_failed(inode);
-              return ERR_PTR(-EIO);
-}
-
-
-static struct inode_operations Inode_Ops_4_Our_Proc_File = {
- .permission = module_permission, /* check for permissions */
-};
-
-static struct file_operations file_ops = {
- .read = procfs_read,
- .write = procfs_write,
- .open = procfs_open,
- .release = procfs_close,
+const struct file_operations keys_fops = {
+	.read = keys_read,
 };
 
 static int __init simple_init(void)
